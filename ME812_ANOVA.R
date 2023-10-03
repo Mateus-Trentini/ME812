58af61faf502d9180c64b8ca2331d013c1419a55
Mateus Trentini <mateustrentini7@gmail.com>
2023-10-03 02:13:50 -0300

    Initial commit for ME812 files

---
+++ ME812_ANOVA.R
@@ -0,0 +1,239 @@
+################################################################################
+##### Potencial de utilização de fósforo de fontes orgânicas em Eucalyptus ##### 
+#####       grandis e E. globulus: influência da micorriza arbuscular      #####
+################################################################################
+#                                                                              #
+# Alunos:                                                                      #
+#   Daniela Konno 169494                                                       #
+#   Jadson Rodrigo Silva de Oliveira 218405                                    #
+#   Maria Julia de Lima Silva 184181                                           #
+#   Mateus Trentini 217290                                                     #
+#   Matheus Eduardo Baruta Lima 241717                                         #
+#                                                                              #
+# Professora Supervisora:                                                      #
+#   Samara Flamini Kiihl                                                       #
+#                                                                              #
+# Pesquisadora:                                                                #
+#   Sara Adrian Lopez de Andrade                                               #
+#                                                                              #
+# Colaborador:                                                                 #
+#   Leonardo Souza de Andrade                                                  #
+#                                                                              #
+################################################################################
+
+# Bibliotecas----
+library(tidyverse)
+library(readxl)
+library(here)
+library(MASS)
+
+
+# Diretorio----
+path <- here() 
+
+
+# Dados----
+dados10 <- read_xlsx(here(path, "Dados.xlsx"), 1)
+dados5 <- read_xlsx(here(path, "Dados.xlsx"), 2)
+
+
+# Fatorial triplo 2 x 7 x 2 com 10 repeticoes---- #
+
+## Antocianinas
+model10_antocianinas <- aov(
+  antocianinas ~ Esp * Micorriza * Fonte, 
+  data = dados10
+)
+
+df <- summary(model10_antocianinas)[[1]][5] %>% 
+  data.frame() %>% 
+  filter(Pr..F. < 0.05) %>% 
+  rownames()
+
+summary(model10_antocianinas)
+
+### tukey
+
+tukey_hsd_esp <- TukeyHSD(model10_antocianinas, "Esp")
+
+# Print the results
+print(tukey_hsd_esp)
+
+# Perform Tukey's HSD test for Micorriza
+tukey_hsd_micorriza <- TukeyHSD(model10_antocianinas, "Micorriza")
+
+# Print the results
+print(tukey_hsd_micorriza)
+
+# Perform Tukey's HSD test for Esp:Micorriza interaction
+tukey_hsd_esp_micorriza <- TukeyHSD(model10_antocianinas, "Esp:Micorriza")
+
+print(tukey_hsd_esp_micorriza)
+
+## Clorofila
+model10_clorofila <- aov(
+  clorofila ~ Esp * Micorriza * Fonte, 
+  data = dados10
+)
+summary(model10_clorofila)
+
+# Perform Tukey's HSD test for Esp
+tukey_hsd_esp_clorofila <- TukeyHSD(model10_clorofila, "Esp")
+print(tukey_hsd_esp_clorofila)
+
+# Perform Tukey's HSD test for Micorriza
+tukey_hsd_micorriza_clorofila <- TukeyHSD(model10_clorofila, "Micorriza")
+print(tukey_hsd_micorriza_clorofila)
+
+# Perform Tukey's HSD test for Fonte
+tukey_hsd_fonte_clorofila <- TukeyHSD(model10_clorofila, "Fonte")
+print(tukey_hsd_fonte_clorofila)
+
+# Perform Tukey's HSD test for Esp:Fonte interaction
+tukey_hsd_esp_fonte_clorofila <- TukeyHSD(model10_clorofila, "Esp:Fonte")
+print(tukey_hsd_esp_fonte_clorofila)
+
+## Flavonois
+model10_flavonois <- aov(
+  flavonois ~ Esp * Micorriza * Fonte, 
+  data = dados10
+)
+summary(model10_flavonois)
+
+# Perform Tukey's HSD test for Esp
+tukey_hsd_esp_flavonois <- TukeyHSD(model10_flavonois, "Esp")
+print(tukey_hsd_esp_flavonois)
+
+# Perform Tukey's HSD test for Micorriza
+tukey_hsd_micorriza_flavonois <- TukeyHSD(model10_flavonois, "Micorriza")
+print(tukey_hsd_micorriza_flavonois)
+
+# Perform Tukey's HSD test for Micorriza:Fonte interaction
+tukey_hsd_micorriza_fonte_flavonois <- TukeyHSD(model10_flavonois, "Micorriza:Fonte")
+print(tukey_hsd_micorriza_fonte_flavonois)
+
+# Perform Tukey's HSD test for Esp:Micorriza interaction
+tukey_hsd_esp_micorriza_flavonois <- TukeyHSD(model10_flavonois, "Esp:Micorriza")
+print(tukey_hsd_esp_micorriza_flavonois)
+
+## NBI
+model10_nbi <- aov(
+  NBI ~ Esp * Micorriza * Fonte, 
+  data = dados10
+)
+summary(model10_nbi)
+
+## Conteudo de potassio (mg) na planta
+model10_conteudo <- aov(
+  `conteudo de P mg planta -1` ~ Esp * Micorriza * Fonte, 
+  data = dados10
+)
+summary(model10_conteudo)
+
+## Concentracao de potassio (g/kg)
+model10_concentracao <- aov(
+  `conc P g/kg` ~ Esp * Micorriza * Fonte, 
+  data = dados10
+)
+summary(model10_concentracao)
+
+## PUE
+model10_pue <- aov(
+  PUE ~ Esp * Micorriza * Fonte, 
+  data = dados10
+)
+summary(model10_pue)
+
+## PUpE
+model10_pupe <- aov(
+  PUpE ~ Esp * Micorriza * Fonte, 
+  data = dados10
+)
+summary(model10_pupe)
+
+
+## tukey ##
+
+
+
+# Fatorial triplo 2 x 7 x 2 com 5 repeticoes----
+
+## Area sup (cm2)
+model5_areasup <- aov(
+  `area sup (cm2)` ~ Esp * Micorriza * Fonte, 
+  data = dados5
+)
+summary(model5_areasup)
+
+## Diametro medio (mm)
+model5_avgdiam <- aov(
+  `AvgDiam(mm)` ~ Esp * Micorriza * Fonte, 
+  data = dados5
+)
+summary(model5_avgdiam)
+
+## Comprimento da raiz
+model5_rootlength <- aov(
+  `root length` ~ Esp * Micorriza * Fonte, 
+  data = dados5
+)
+summary(model5_rootlength)
+
+## Peso da raiz seca (g)
+model5_rootdryweight <- aov(
+  `root dry weight (g)` ~ Esp * Micorriza * Fonte, 
+  data = dados5
+)
+summary(model5_rootdryweight)
+
+## Peso do broto seco (g)
+model5_dryweightshoot <- aov(
+  `dry weight shoot (g)` ~ Esp * Micorriza * Fonte, 
+  data = dados5
+)
+summary(model5_dryweightshoot)
+
+## Contagem
+model5_countn <- aov(
+  `count N` ~ Esp * Micorriza * Fonte, 
+  data = dados5
+)
+summary(model5_countn)
+
+## Fitase
+model5_fitase <- aov(
+  `Fitase ng Pi/cm2/min` ~ Esp * Micorriza * Fonte, 
+  data = dados5
+)
+summary(model5_fitase)
+
+## rApase
+model5_rapase <- aov(
+  `rAPase` ~ Esp * Micorriza * Fonte, 
+  data = dados5
+)
+summary(model5_rapase)
+
+
+### manova ####
+
+
+# Specify the dependent variables individually
+antocianinas <- dados10$antocianinas
+clorofila <- dados10$clorofila
+flavonois <- dados10$flavonois
+NBI <- dados10$NBI
+conteudo_de_P_mg_planta_1 <- dados10$`conteudo de P mg planta -1`
+conc_P_g_kg <- dados10$`conc P g/kg`
+PUE <- dados10$PUE
+PUpE <- dados10$PUpE
+fosfatase_micélio <- dados10$`fosfatase micélio`
+
+# Perform MANOVA
+manova_result <- manova(cbind(
+  antocianinas, clorofila, flavonois, NBI
+) ~ Esp * Fonte * Micorriza, data = dados10)
+
+# Summary of MANOVA
+summary(manova_result)
+
